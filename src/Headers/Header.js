cab1e0fc5fa15079f5496ad03c3d1a6bd7b05ce2
SimonBFornillos <simonbfornillos@gmail.com>
2023-09-13 18:51:16 +0800

    removed S from headers

--- src/Headers/Header.js
+++ src/Headers/Header.js
@@ -51,7 +51,7 @@ const Headermain = () => {
             </div>
           </div>
           <div className="menu_footer d-flex flex-column flex-md-row justify-content-between align-items-md-center position-absolute w-100 p-3">
-            <div className="d-flex">s
+            <div className="d-flex">
             <a href={socialprofiles.linkedin}>LinkedIn</a>
             <a href={socialprofiles.github}>Github</a>
             </div>
